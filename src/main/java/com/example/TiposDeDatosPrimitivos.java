a37d2603e5c8feb4ac39fce0fb9fb7e8f0b91296
Sofia Solorzano <solorzanos213@gmail.com>
2026-03-05 18:48:53 -0500

    Agregando clases faltantes

---
+++ src/main/java/com/example/TiposDeDatosPrimitivos.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class TiposDeDatosPrimitivos {
+
+    public static void ejemplo() {
+        int numero = 10;
+        double decimal = 5.5;
+        char letra = 'A';
+        boolean activo = true;
+
+        System.out.println("Entero: " + numero);
+        System.out.println("Decimal: " + decimal);
+        System.out.println("Letra: " + letra);
+        System.out.println("Booleano: " + activo);
+    }
+
+}
